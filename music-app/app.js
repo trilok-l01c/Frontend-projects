944b646c251f37628668805adc51e4bac8088944
Trilok Lowanshi <trilokl01c@gmail.com>
2025-12-03 17:48:19 +0530

    Removed music-app (moved to standalone)

--- music-app/app.js
+++
@@ -1,6 +0,0 @@
-fetch("https://api.spotify.com/v1/search?q=shayad&type=track&limit=1", {
-    headers: { Authorization: "Bearer YOUR_ACCESS_TOKEN" },
-})
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch(console.error);
