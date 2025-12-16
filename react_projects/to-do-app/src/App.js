4bc0d162c546b85b742b642105138f79f2d4a09c
Trilok Lowanshi <trilokl01c@gmail.com>
2025-12-16 22:10:16 +0530

    Added next for starting and practising

--- react_projects/to-do-app/src/App.js
+++ react_projects/to-do-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [text, setText] = useState("");
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+    return (
+        <div
+            className="App"
+            style={{
+                width: "90vmax",
+                height: "40vmin",
+                borderRadius: "4px",
+                backgroundColor: "#ea6456",
+                margin: "100px auto",
+                padding: "10px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <input
+                style={{
+                    width: "150px",
+                    height: "30px",
+                    textAlign: "center",
+                }}
+                onChange={(e) => {
+                    setText(e.target.value);
+                }}
+                ref={inputRef}
+            />
+            <p>The text is {text}</p>
+        </div>
+    );
 }
 
 export default App;
